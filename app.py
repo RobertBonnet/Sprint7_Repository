ac246b378e6272996026b6474e4c0f35c0e3126e
Robert Bonnet <robert.bonnet@Robert-Bonnet.local>
2025-01-17 14:24:24 -0600

    app.py corrected

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-car_data = pd.read_csv('../vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 # Título de la aplicación
 st.title("Cuadro de Mandos de Análisis de Datos de Coches")
